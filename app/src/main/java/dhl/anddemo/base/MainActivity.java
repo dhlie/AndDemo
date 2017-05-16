d7b9c763de04248f59313034d357a7daaaaa81e7
DuanHl <dhlie@163.com>
2017-05-16 14:25:57 +0800

    clip region

--- app/src/main/java/dhl/anddemo/base/MainActivity.java
+++ app/src/main/java/dhl/anddemo/base/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import dhl.anddemo.R;
+import dhl.anddemo.clipregion.ClipRegionActivity;
 import dhl.anddemo.matrix.CameraApiActivity;
 
 public class MainActivity extends BaseActivity {
@@ -25,6 +26,9 @@ public class MainActivity extends BaseActivity {
         switch (view.getId()) {
             case R.id.tv_camera_matrix:
                 startActivity(new Intent(getApplicationContext(), CameraApiActivity.class));
+                break;
+            case R.id.tv_clipregion:
+                startActivity(new Intent(getApplicationContext(), ClipRegionActivity.class));
                 break;
         }
     }
---
+++ app/src/main/java/dhl/anddemo/clipregion/ClipRegionActivity.java
@@ -0,0 +1,149 @@
+package dhl.anddemo.clipregion;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.Region;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import dhl.anddemo.R;
+import dhl.anddemo.base.BaseActivity;
+import dhl.anddemo.base.TitleBar;
+
+/**
+ * Created by DuanHl on 2017/5/16.
+ */
+
+public class ClipRegionActivity extends BaseActivity {
+
+    private TextView mTVOP;
+    private ClipRegionView mClipView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clipregion_activity);
+
+        TitleBar.SimpleTitleBarClickListener titleBarClickListener = new TitleBar.SimpleTitleBarClickListener() {
+            @Override
+            public void onLeftClick(View v) {
+                finish();
+            }
+        };
+        TitleBar titleBar = (TitleBar) findViewById(R.id.titlebar);
+        titleBar.setTitleBarClickListener(titleBarClickListener);
+        titleBar.setTitle(getClass().getSimpleName());
+
+        mTVOP = (TextView) findViewById(R.id.tv_op);
+        FrameLayout fl = (FrameLayout) findViewById(R.id.view_clip);
+        mClipView = new ClipRegionView(this);
+        mClipView.setOP(Region.Op.UNION);
+        fl.addView(mClipView);
+    }
+
+    public void onClick(View view) {
+        Button btn = (Button) view;
+        String text = btn.getText().toString();
+        mTVOP.setText(text);
+        Region.Op op = null;
+        if (text.contains(".DIFFERENCE")) {
+            op = Region.Op.DIFFERENCE;
+        } else if (text.contains("REVERSE_DIFFERENCE")) {
+            op = Region.Op.REVERSE_DIFFERENCE;
+        } else if (text.contains("REPLACE")) {
+            op = Region.Op.REPLACE;
+        } else if (text.contains("INTERSECT")) {
+            op = Region.Op.INTERSECT;
+        } else if (text.contains("UNION")) {
+            op = Region.Op.UNION;
+        } else if (text.contains("XOR")) {
+            op = Region.Op.XOR;
+        }
+        mClipView.setOP(op);
+        mClipView.invalidate();
+    }
+
+    public static class ClipRegionView extends View {
+
+        private Paint mPaint;
+        private Rect mRectOne = new Rect();
+        private int mRadius;
+        private int mCenterX, mCenterY;
+        private Region.Op mOP;
+        private Path mPath;
+
+        public ClipRegionView(Context context) {
+            super(context);
+
+            mPaint = new Paint();
+            mPaint.setAntiAlias(true);
+            mPaint.setStrokeWidth(6);
+            mPaint.setTextSize(getResources().getDimensionPixelSize(R.dimen.titlebar_title_textsize));
+            mPath = new Path();
+        }
+
+        public void setOP(Region.Op op) {
+            mOP = op;
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            int rectWidt = w/4;
+            mRectOne.set(rectWidt, h/2 - rectWidt, w - rectWidt, h/2 + rectWidt);
+            mRadius = w/4;
+            mCenterX = w - mRadius;
+            mCenterY = h - mRadius;
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.save();
+            canvas.clipRect(mRectOne);
+            mPath.reset();
+            mPath.addCircle(mCenterX, mCenterY, mRadius, Path.Direction.CCW);
+            canvas.clipPath(mPath, mOP);
+            drawRect(canvas);
+            drawCircle(canvas);
+            canvas.restore();
+        }
+
+        private void drawRect(Canvas canvas) {
+            mPaint.setColor(Color.RED);
+            canvas.drawRect(mRectOne, mPaint);
+        }
+        private void drawCircle(Canvas canvas) {
+            mPaint.setColor(Color.BLUE);
+            canvas.drawCircle(mCenterX, mCenterY, mRadius, mPaint);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    getParent().requestDisallowInterceptTouchEvent(true);
+                    mCenterX = (int) event.getX();
+                    mCenterY = (int) event.getY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    mCenterX = (int) event.getX();
+                    mCenterY = (int) event.getY();
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    mCenterX = (int) event.getX();
+                    mCenterY = (int) event.getY();
+                    break;
+            }
+            invalidate();
+            return true;
+        }
+    }
+}
