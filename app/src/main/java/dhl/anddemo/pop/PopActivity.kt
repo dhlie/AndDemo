3369543fe06832a965df5d54979ac7e4350cd8a1
duanhaoliang <haoliang_duan@lebo.cn>
2025-09-17 17:22:36 +0800

    popActivity

--- app/src/main/java/dhl/anddemo/pop/PopActivity.kt
+++ app/src/main/java/dhl/anddemo/pop/PopActivity.kt
@@ -1,11 +1,14 @@
 package dhl.anddemo.pop
 
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams
 import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.core.view.updateLayoutParams
 import com.dhl.base.SingleClickListener
 import com.dhl.base.dp
 import com.dhl.base.pop.BasePop
+import com.dhl.base.screenSize
 import dhl.anddemo.base.BuBaseActivity
 import dhl.anddemo.databinding.ActiPopBinding
 
@@ -27,6 +30,9 @@ class PopActivity : BuBaseActivity() {
         binding.titleBar.ivBack?.setOnClickListener {
             finish()
         }
+        binding.vTop.updateLayoutParams {
+            height = screenSize.y * 2 / 3
+        }
         initPopWindow()
     }
 
@@ -34,29 +40,29 @@ class PopActivity : BuBaseActivity() {
         val clickListener = SingleClickListener {
             when (it) {
                 binding.btnPopTop -> {
-                    val pop = getPop(BasePop.TrianglePlacement.BOTTOM)
+                    val pop = getPop(BasePop.TrianglePlacement.BOTTOM, 70.dp, 120.dp, 0xffffffff.toInt(), 0xff464343.toInt())
                     pop.showAtLocationUp(
                         it,
                         -1,
                         -1,
-                        -16.dp,
+                        (-16).dp,
                         12.dp
                     )
                 }
 
                 binding.btnPopBottom -> {
-                    val pop = getPop(BasePop.TrianglePlacement.TOP)
+                    val pop = getPop(BasePop.TrianglePlacement.TOP, 320.dp, 70.dp, 0xffffffff.toInt(), 0xff464343.toInt())
                     pop.showAtLocationDown(
                         it,
                         -1,
                         -1,
-                        -16.dp,
+                        0,
                         16.dp
                     )
                 }
 
                 binding.btnPopLeft -> {
-                    val pop = getPop(BasePop.TrianglePlacement.END)
+                    val pop = getPop(BasePop.TrianglePlacement.END, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
                     pop.showAtLocationLeft(
                         it,
                         -1,
@@ -67,7 +73,7 @@ class PopActivity : BuBaseActivity() {
                 }
 
                 binding.btnPopRight -> {
-                    val pop = getPop(BasePop.TrianglePlacement.START)
+                    val pop = getPop(BasePop.TrianglePlacement.START, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
                     pop.showAtLocationRight(
                         it,
                         -1,
@@ -84,15 +90,15 @@ class PopActivity : BuBaseActivity() {
         binding.btnPopRight.setOnClickListener(clickListener)
     }
 
-    private fun getPop(trianglePlacement: BasePop.TrianglePlacement): BasePop {
+    private fun getPop(trianglePlacement: BasePop.TrianglePlacement, width: Int, height: Int, textColor: Int = getColor(com.dhl.base.R.color.text_primary), bgColor: Int = getColor(com.dhl.base.R.color.bg_secondary)): BasePop {
         val textView = TextView(this)
         textView.setPadding(16.dp, 16.dp, 16.dp, 16.dp)
-        textView.layoutParams = FrameLayout.LayoutParams(70.dp, 120.dp)
+        textView.layoutParams = FrameLayout.LayoutParams(width, height)
         textView.text = "测试PopupWindow测试PopupWindow测试PopupWindow测试PopupWindow"
-        textView.setTextColor(getColor(com.dhl.base.R.color.text_primary))
+        textView.setTextColor(textColor)
 
         return BasePop.Builder().setTrianglePlacement(trianglePlacement)
-            .setBackgroundColor(getColor(com.dhl.base.R.color.bg_secondary))
+            .setBackgroundColor(bgColor)
             .setContentView(textView)
             .build(this)
     }
